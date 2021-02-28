21f20720a751e22a0c3bcad738f076e215e0000a
Boldizsár Tóth <bocko200@gmail.com>
2021-02-28 23:55:50 +0100

    Main menu design changes and center buttons.

--- LogicGames/Menus/MainMenu.Designer.cs
+++ LogicGames/Menus/MainMenu.Designer.cs
@@ -28,60 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.gameTetris = new System.Windows.Forms.Button();
-            this.gameMinesweeper = new System.Windows.Forms.Button();
-            this.game2048 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // gameTetris
-            // 
-            this.gameTetris.Location = new System.Drawing.Point(302, 225);
-            this.gameTetris.Name = "gameTetris";
-            this.gameTetris.Size = new System.Drawing.Size(122, 45);
-            this.gameTetris.TabIndex = 1;
-            this.gameTetris.Text = "Tetris";
-            this.gameTetris.UseVisualStyleBackColor = true;
-            this.gameTetris.Click += new System.EventHandler(this.gameTetris_Click);
-            // 
-            // gameMinesweeper
-            // 
-            this.gameMinesweeper.Location = new System.Drawing.Point(302, 276);
-            this.gameMinesweeper.Name = "gameMinesweeper";
-            this.gameMinesweeper.Size = new System.Drawing.Size(122, 45);
-            this.gameMinesweeper.TabIndex = 2;
-            this.gameMinesweeper.Text = "Aknakereső";
-            this.gameMinesweeper.UseVisualStyleBackColor = true;
-            this.gameMinesweeper.Click += new System.EventHandler(this.gameMinesweeper_Click);
-            // 
-            // game2048
-            // 
-            this.game2048.Location = new System.Drawing.Point(302, 174);
-            this.game2048.Name = "game2048";
-            this.game2048.Size = new System.Drawing.Size(122, 45);
-            this.game2048.TabIndex = 0;
-            this.game2048.Text = "2048";
-            this.game2048.UseVisualStyleBackColor = true;
-            this.game2048.Click += new System.EventHandler(this.game2048_Click);
             // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(717, 535);
-            this.Controls.Add(this.game2048);
-            this.Controls.Add(this.gameMinesweeper);
-            this.Controls.Add(this.gameTetris);
+            this.ClientSize = new System.Drawing.Size(782, 853);
+            this.MinimumSize = new System.Drawing.Size(800, 900);
             this.Name = "MainMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "KANCHO GAMES";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button gameTetris;
-        private System.Windows.Forms.Button gameMinesweeper;
-        private System.Windows.Forms.Button game2048;
     }
 }
 
--- LogicGames/Menus/MainMenu.cs
+++ LogicGames/Menus/MainMenu.cs
@@ -15,14 +15,57 @@ namespace LogicGames.Menus
 {
     public partial class MainMenu : Form
     {
+        private Panel panel;
+        private Size buttonSize = new Size(150, 50);
+        private int padding = 10;
         public MainMenu()
         {
             InitializeComponent();
+            CreateButtonPanel();
+            this.BackColor = Resources.Colors.Background;
+            this.Resize += OnResize;
         }
-        
+
+        private void CreateButtonPanel()
+        {
+            panel = new Panel();
+            panel.Size = new Size(buttonSize.Width, 0);
+            panel.Location = new Point(100, 100);
+            CreateButton(panel, "2048", game2048_Click);
+            CreateButton(panel, "Tetris", gameTetris_Click);
+            CreateButton(panel, "Aknakereső", gameMinesweeper_Click);
+            CenterButtonPanel();
+            this.Controls.Add(panel);
+        }
+
+        private void CreateButton(Panel panel, string text, EventHandler onClick)
+        {
+            Button button = new Button();
+            button.Text = text;
+            button.Font = new Font("Arial", 12, FontStyle.Bold);
+            button.BackColor = Resources.Colors.ContainterBackground;
+            button.ForeColor = Resources.Colors.PrimaryText;
+            button.FlatStyle = new FlatStyle();
+            button.FlatAppearance.BorderSize = 0;
+            button.Click += onClick;
+            button.Size = buttonSize;
+            panel.Controls.Add(button);
+            button.Location = new Point(0, panel.Height);
+            panel.Height += buttonSize.Height+padding;
+        }
+
+        private void CenterButtonPanel()
+        {
+            panel.Location = new Point
+            (
+                (this.ClientRectangle.Width / 2) - (panel.Width / 2),
+                (this.ClientRectangle.Height / 2) - (panel.Height / 2)
+            );
+        }
+
         private void gameClosed(object sender, FormClosedEventArgs e)
         {
-            
+            CreateButtonPanel();
         }
 
         private void showGame(Form gameForm)
@@ -51,6 +94,11 @@ namespace LogicGames.Menus
         private void gameMinesweeper_Click(object sender, EventArgs e)
         {
             showGame(new Minesweeper());
+        }
+
+        private void OnResize(object sender, EventArgs e)
+        {
+            CenterButtonPanel();
         }
     }
 }
